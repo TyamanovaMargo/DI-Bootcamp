1f431e6c54e17b82a2bb1a8857b5fcb194c8d172
Margo Tiamanova <margotiamanova@Margos-MacBook-Pro.local>
2025-02-02 16:25:15 +0200

    add exercise dailyChallenge

---
+++ Week1/Day1/DailyChallenge/build_up_string.py
@@ -0,0 +1,9 @@
+answer_from_user = input("Give me a string:")
+letter_num = ""
+for letter in   answer_from_user :
+        letter_num = letter_num + letter[0]
+        print(letter_num)
+        
+
+
+
