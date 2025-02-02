563ae66fd69db48059a570cea9e71c94b07ac70f
Margo Tiamanova <margotiamanova@Margos-MacBook-Pro.local>
2025-02-02 16:39:02 +0200

    add exercise dailyChallenge

--- Week1/Day1/DailyChallenge/build_up_string.py
+++ Week1/Day1/DailyChallenge/build_up_string.py
@@ -1,16 +1,16 @@
 answer_from_user = input("Give me a string:")
-letter_num = ""
+letter_new = ''
+
 if len(answer_from_user) < 10 :
         print('string not long enough')
 elif len(answer_from_user) > 10 :
         print('string too long')
 else: 
       print('Perfect string')
-      print(f' First letter: {answer_from_user[0]}')
-      print(f' Last letter: {answer_from_user[9:]}')
+      print(f' First letter: {answer_from_user[0]} and Last letter: {answer_from_user[9:]}')
       for letter in   answer_from_user :
-        letter_num = letter_num + letter[0]
-        print(letter_num)
+        letter_new = letter_new + letter[0]
+        print(letter_new)
         
 
 
