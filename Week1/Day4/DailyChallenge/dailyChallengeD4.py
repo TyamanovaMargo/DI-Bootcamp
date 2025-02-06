8c228534a4dcaca32aa4be0733aa1b36e400001f
Margo Tiamanova <margotiamanova@Margos-MacBook-Pro.local>
2025-02-06 02:56:04 +0200

    done

---
+++ Week1/Day4/DailyChallenge/dailyChallengeD4.py
@@ -0,0 +1,32 @@
+#=== Instructions ==========
+
+matrix = [
+    ['7', 'i', 'i'],
+    ['T', 's', 'x'],
+    ['h', '%', '?'],
+    ['i', '', '#'],
+    ['s', 'M', ''],
+    ['$', 'a', ''],
+    ['#', 't', '%'],
+    ['^', 'r', '!']
+]
+
+                                  
+matrix_grid = [''.join(row) for row in matrix]
+
+new_matrix = []
+for column in range(len(matrix[0])): 
+    for row in matrix_grid:  
+        if column < len(row): 
+            char = row[column]
+            print(char)
+            if char.isalpha():  # check alphabet char or not
+                new_matrix.append(char)
+                print(new_matrix)
+            elif new_matrix and new_matrix[-1] != " ":
+                new_matrix.append(" ") #change symbol to space
+                print(new_matrix)
+
+# delete space
+coding_message = ''.join(new_matrix).strip()
+print(coding_message)
