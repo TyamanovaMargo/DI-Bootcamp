5919531c0fe1ac0470a2be63d5a799f43fb96141
Margo Tiamanova <margotiamanova@Margos-MacBook-Pro.local>
2025-02-06 03:00:37 +0200

    done

--- Week1/Day4/DailyChallenge/dailyChallengeD4.py
+++ Week1/Day4/DailyChallenge/dailyChallengeD4.py
@@ -13,19 +13,20 @@ matrix = [
 
                                   
 matrix_grid = [''.join(row) for row in matrix]
+print(matrix_grid)
 
 new_matrix = []
 for column in range(len(matrix[0])): 
     for row in matrix_grid:  
         if column < len(row): 
             char = row[column]
-            print(char)
+            # print(char)
             if char.isalpha():  # check alphabet char or not
                 new_matrix.append(char)
-                print(new_matrix)
+                # print(new_matrix)
             elif new_matrix and new_matrix[-1] != " ":
                 new_matrix.append(" ") #change symbol to space
-                print(new_matrix)
+                # print(new_matrix)
 
 # delete space
 coding_message = ''.join(new_matrix).strip()
