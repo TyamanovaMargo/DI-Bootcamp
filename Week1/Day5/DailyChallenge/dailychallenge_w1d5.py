252f5b271386c92e490313b420248b0669f61f30
Margo Tiamanova <margotiamanova@Margos-MacBook-Pro.local>
2025-02-08 13:24:19 +0200

    done this challenge

---
+++ Week1/Day5/DailyChallenge/dailychallenge_w1d5.py
@@ -0,0 +1,30 @@
+#========= Challenge 1 : Sorting ==========
+'''
+
+    Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+    Use List Comprehension
+'''
+
+word_from_user = input("Enter a comma-separated sequence of words: ") # also can split here
+
+
+result = [ word for word in word_from_user.split(",")] # Split the input 'items' into a list of words
+
+print(",".join(sorted(list(result))))
+
+
+# =========== Challenge 2 : Longest Word ==========
+
+'''
+Write a function that finds the longest word in a sentence. If two or more words are found, return the first longest word.
+Characters such as apostrophe, comma, period count as part of the word (e.g. O’Connor is 8 characters long).
+'''
+
+sent = input("Enter a sentence: ")
+def longest_word (sentence):
+    for word in sentence:
+        print(word,len(word))
+
+
+
+longest_word (sent.split(" ")) #Split into individual words
---
+++ Week1/Day5/MiniProject_w1d5/mini_project_w1d5.py
@@ -0,0 +1,21 @@
+
+display_board(): # – To display the Tic Tac Toe board (GUI).
+    pass
+
+
+
+
+player_input(player) # – To get the position from the player.
+
+
+
+
+check_win()#  – To check whether there is a winner or not.
+
+
+
+
+
+play() # – The main function, which calls all the functions created above.
+
+
